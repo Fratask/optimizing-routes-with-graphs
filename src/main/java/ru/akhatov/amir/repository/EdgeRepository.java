368acb42f1b22fb6262fa5b80dabc555cd18778f
Fratask <amir.akhatov99@gmail.com>
2021-01-16 16:00:11 +0300

    Deleted all for create another implementation of A-star search

--- src/main/java/ru/akhatov/amir/repository/EdgeRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.akhatov.amir.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.akhatov.amir.model.entity.Edge;
-
-@Repository
-public interface EdgeRepository extends JpaRepository<Edge, Long> {
-}
